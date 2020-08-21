2bc5b1a637124b2b033952963abf6ce7ed7c28e5
Bhupendra1234 <bhupendradewangan1998@gmail.com>
2020-08-21 17:50:01 +0530

    commit

---
+++ src/Components/Weather.js
@@ -0,0 +1,32 @@
+import React from 'react';
+const Weather =(props)=>
+{
+
+
+        
+ const minimumTemp =(min,max) =>
+{
+        return (
+                <h3>
+                        <span className="px-4">{min}&deg;</span>
+                        <span className="px-4">{max}&deg;</span>
+                </h3>
+        );
+}
+     return <div className="container"> 
+     <div className="cards">
+        <h1>{props.city},{props.country}</h1>
+        <h5 className="py-4">
+        <i className={`wi${props.weatherIcon}display-1`}></i>
+        </h5>
+        <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+         { minimumTemp(props.temp_min,props.temp_max)}
+        <h4 className="py-3">{props.description}</h4>
+        </div>
+        </div>
+        
+}
+
+
+
+export default Weather;
