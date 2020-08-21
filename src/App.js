e02fd9a9deb4ce60afa217817ba36b811c8f46a0
Bhupendra1234 <bhupendradewangan1998@gmail.com>
2020-08-21 11:18:25 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App =()=>
+{
+     return <div className="container">
+        <h1>Weather App</h1>
+     </div>
+}
+
+export default App;
